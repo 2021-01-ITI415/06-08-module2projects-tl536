2677af4b3c13240ca60577fc0b6d069f7c118951
tl536 <tl536@scarletmail.rutgers.edu>
2021-03-11 16:48:16 -0500

    create an enum, which defines a type of variable that only has a few possible named values

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// An enum defines a variable type with a few prenamed values // a
+public enum eCardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+public class CardProspector : Card
+{ // Make sure CardProspector extends Card
+    [Header("Set Dynamically: CardProspector")]
+    // This is how you use the enum eCardState
+    public eCardState state = eCardState.drawpile;
+    // The hiddenBy list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+
+public int layoutID;
+    // The SlotDef class stores information pulled in from the LayoutXML <slot>
+    public SlotDef slotDef;
+}
