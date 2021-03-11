063028415c67899a84c1181f644bf6aaccf08bb3
tl536 <tl536@scarletmail.rutgers.edu>
2021-03-11 16:44:49 -0500

    now the <slot>s have been parsed from the XML

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -11,10 +11,12 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
-
+	public TextAsset layoutXML;
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+	public Layout layout;
+
 
 	void Awake(){
 		S = this;
@@ -23,13 +25,15 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
-		Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference // a
+		Deck.Shuffle(ref deck.cards); 
 		Card c;
 		for (int cNum = 0; cNum < deck.cards.Count; cNum++)
-		{ // b
+		{ 
 			c = deck.cards[cNum];
 			c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
 		}
+		layout = GetComponent<Layout>(); 
+		layout.ReadLayout(layoutXML.text);
 	}
 
 }
