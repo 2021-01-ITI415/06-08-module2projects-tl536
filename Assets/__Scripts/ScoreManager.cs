8097731f6fc54e810be170512b541645dfa6ec87
tl536 <tl536@scarletmail.rutgers.edu>
2021-03-11 22:40:09 -0500

    For some reason the score is not working properly, so I deleted it

--- Assets/__Scripts/ScoreManager.cs
+++ Assets/__Scripts/ScoreManager.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 // An enum to handle all the possible scoring events
 public enum eScoreEvent
 {
     draw,
     mine,
-    mineGold,
-    gameWin,
+    mineGold, gameWin,
     gameLoss
 }
-// ScoreManager handles all of the scoring
+
 public class ScoreManager : MonoBehaviour
-{ // a
-    static private ScoreManager S; // b
+{
+    static private ScoreManager S;
+
     static public int SCORE_FROM_PREV_ROUND = 0;
     static public int HIGH_SCORE = 0;
     [Header("Set Dynamically")]
@@ -24,34 +25,39 @@ public class ScoreManager : MonoBehaviour
     void Awake()
     {
         if (S == null)
-        { // c
-            S = this; // Set the private singleton
+        {
+            S = this;
         }
         else
         {
             Debug.LogError("ERROR: ScoreManager.Awake(): S is already set!");
         }
+
         // Check for a high score in PlayerPrefs
         if (PlayerPrefs.HasKey("ProspectorHighScore"))
         {
             HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
         }
+
         // Add the score from last round, which will be >0 if it was a win
         score += SCORE_FROM_PREV_ROUND;
         // And reset the SCORE_FROM_PREV_ROUND
         SCORE_FROM_PREV_ROUND = 0;
     }
-static public void EVENT(eScoreEvent evt)
-    { // d
+
+    static public void EVENT(eScoreEvent evt)
+    {
+        // try-catch stops an error from breaking the progam
         try
-        { // try-catch stops an error from breaking your program
+        {
             S.Event(evt);
         }
         catch (System.NullReferenceException nre)
         {
-            Debug.LogError ("ScoreManager:EVENT() called while S=null.\n" + nre );
+            Debug.LogError("ScoreManager:EVENT() called while S=null. \n" + nre);
         }
     }
+
     void Event(eScoreEvent evt)
     {
         switch (evt)
@@ -60,8 +66,7 @@ static public void EVENT(eScoreEvent evt)
             case eScoreEvent.draw: // Drawing a card
             case eScoreEvent.gameWin: // Won the round
             case eScoreEvent.gameLoss: // Lost the round
-                chain = 0; // resets the score chain
-                score += scoreRun; // add scoreRun to total score
+                chain = 0; // resets the score chainscore += scoreRun; // add scoreRun to total score
                 scoreRun = 0; // reset scoreRun
                 break;
             case eScoreEvent.mine: // Remove a mine card
@@ -69,17 +74,14 @@ static public void EVENT(eScoreEvent evt)
                 scoreRun += chain; // add score for this card to run
                 break;
         }
-        // This second switch statement handles round wins and losses
         switch (evt)
         {
             case eScoreEvent.gameWin:
-                // If it's a win, add the score to the next round
-                // static fields are NOT reset by SceneManager.LoadScene()
                 SCORE_FROM_PREV_ROUND = score;
                 print("You won this round! Round score: " + score);
                 break;
+
             case eScoreEvent.gameLoss:
-                // If it's a loss, check against the high score
                 if (HIGH_SCORE <= score)
                 {
                     print("You got the high score! High score: " + score);
@@ -91,12 +93,14 @@ static public void EVENT(eScoreEvent evt)
                     print("Your final score for the game was: " + score);
                 }
                 break;
+
             default:
                 print("score: " + score + " scoreRun:" + scoreRun + " chain:" + chain);
                 break;
         }
     }
-    static public int CHAIN { get { return S.chain; } } // e
+
+    static public int CHAIN { get { return S.chain; } }
     static public int SCORE { get { return S.score; } }
     static public int SCORE_RUN { get { return S.scoreRun; } }
 }
